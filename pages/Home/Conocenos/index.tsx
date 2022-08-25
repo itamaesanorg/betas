657065f8e0f92c24743b7d490d4574359f641aaf
Miguel Gargallo <mgargallo16@gmail.com>
2022-08-25 22:18:59 +0200

    1.2.2

---
+++ pages/Home/Conocenos/index.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from 'next'
+import MainMenu from '/components/common/01 Head/01 MainMenu/MainMenu'
+
+const Home: NextPage = () => {
+  return (
+    <MainMenu />
+  )
+}
+
+export default Home
