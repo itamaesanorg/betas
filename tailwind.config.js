0151014f16a50507150e1eff43d304c4eb940542
Miguel Gargallo <mgargallo16@gmail.com>
2022-08-26 21:05:37 +0200

    1.0.1 Corte

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('/img/hero-pattern.svg')",
+        'footer-texture': "url('/img/footer-texture.png')",
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/aspect-ratio'),
