fd5f282f5698084bd733a2b6e2abe753b3513be0
Miguel Gargallo <mgargallo16@gmail.com>
2022-08-26 14:55:02 +0200

    1.0.0

--- components/uncommon/02 Services/01 In-Store/02 Body/03 Brand/brand.tsx
+++ components/uncommon/02 Services/01 In-Store/02 Body/03 Brand/brand.tsx
@@ -1,27 +1,31 @@
-/* This example requires Tailwind CSS v2.0+ */
+import Image from 'next/image'
+
 export default function brand() {
   return (
     <div className="bg-white">
       <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
         <div className="grid grid-cols-2 gap-8 md:grid-cols-6 lg:grid-cols-5">
           <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
-            <img className="h-12" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Adidas_Logo.svg/800px-Adidas_Logo.svg.png" alt="Adidas" />
+            <Image className="h-12" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Adidas_Logo.svg/800px-Adidas_Logo.svg.png"
+            width={200} height={200}
+            height={200}
+            alt="Adidas" />
           </div>
           <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
-            <img className="h-12" src="https://upload.wikimedia.org/wikipedia/it/thumb/4/48/Bacardi_Logo.svg/500px-Bacardi_Logo.svg.png?20131016104052" alt="Bacardi" />
+            <Image className="h-12" src="https://upload.wikimedia.org/wikipedia/it/thumb/4/48/Bacardi_Logo.svg/500px-Bacardi_Logo.svg.png?20131016104052" alt="Bacardi" />
           </div>
           <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
-            <img className="h-12" src="https://tailwindui.com/img/logos/statickit-logo-gray-400.svg" alt="StaticKit" />
+            <Image className="h-12" src="https://tailwindui.com/img/logos/statickit-logo-gray-400.svg" alt="StaticKit" />
           </div>
           <div className="col-span-1 flex justify-center md:col-span-3 lg:col-span-1">
-            <img
+            <Image
               className="h-12"
               src="https://tailwindui.com/img/logos/transistor-logo-gray-400.svg"
               alt="Transistor"
             />
           </div>
           <div className="col-span-2 flex justify-center md:col-span-3 lg:col-span-1">
-            <img
+            <Image
               className="h-12"
               src="https://tailwindui.com/img/logos/workcation-logo-gray-400.svg"
               alt="Workcation"
--- components/uncommon/02 Services/05 Otros-Eventos/02 Body/04 info/info.tsx
+++ components/uncommon/02 Services/05 Otros-Eventos/02 Body/04 info/info.tsx
@@ -4,19 +4,39 @@ export default function info() {
   return (
     <div className='relative bg-white dark:bg-white pt-16 px-4 sm:px-6 lg:pt-12 lg:px-8'>
       <div className='bg-white dark:bg-white h-1/3 sm:h-2/3' />
-      <div className='relative max-w-7xl mx-auto'>
-        <div className='text-center'>
-          <h2 className='text-3xl tracking-tight font-extrabold sm:text-4xl dark:text-black'>
-          Otros Servicios
+      <div className='relative max-w-7xl mx-auto text-center'>
+      <h2 className='py-8 text-3xl tracking-tight font-extrabold sm:text-4xl dark:text-black'>
+          Oros Servicios
           </h2>
-          <div className="columns-3 ...">
-  <img className="w-full aspect-video ..." src="https://bcnhostess.com/wp-content/uploads/misteryshooper-1-300x200.jpeg" />
-  <h2>Hola</h2><p> Perfil estratégico en el sector Retail, esencial no solo para supervisar el trabajo del personal, sino también para recuperar información importante acerca del visual merchandising, volumen de ventas, afluencia de clientes. No es un método de control, sino un mecanismo de mejora en el Punto de Venta.</p>
-  <img className="w-full aspect-square ..." src="https://bcnhostess.com/wp-content/uploads/adj-brand-ambassador.jpg" />
-  <img className="w-full aspect-square ..." src="https://bcnhostess.com/wp-content/uploads/adj-accose.jpg" />
-  <img className="w-full aspect-square ..." src="https://bcnhostess.com/wp-content/uploads/adj-vip.jpg" />
-  <img className="w-full aspect-square ..." src="https://bcnhostess.com/wp-content/uploads/adj-stokerimage009.jpg" />
-</div>
+        <div className='py-4 columns-2 text-center'>
+        <h3 className='py-2 text-3xl tracking-tight font-bold sm:text-2xl text-black dark:text-black'>
+          Mistery Shopper
+          </h3>
+          <p className='px-4 sm:px-6 lg:px-8 mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4'>
+          Perfil estratégico en el sector Retail, esencial no solo para supervisar el trabajo del personal, sino también para recuperar información importante acerca del visual merchandising, volumen de ventas, afluencia de clientes. No es un método de control, sino un mecanismo de mejora en el Punto de Venta.
+          </p>
+          <h3 className='py-2 text-3xl tracking-tight font-bold sm:text-2xl text-black dark:text-black'>
+          Brand Ambassador
+          </h3>
+          <p className='px-4 sm:px-6 lg:px-8 mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4'>
+          Es un perfil especializado que permite ganar el reconocimiento, credibilidad y confianza de vuestra marca, para así conseguir la visibilidad y presencia deseada.
+          </p>
+          <h3 className='py-2 text-3xl tracking-tight font-bold sm:text-2xl text-black dark:text-black'>
+          Personal de control de acceso
+          </h3>
+          <p className='px-4 sm:px-6 lg:px-8 mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4'>
+          Personal que controla accesos de fiestas, eventos, ferias, locales nocturnos.
+          </p>
+          <h3 className='py-2 text-3xl tracking-tight font-bold sm:text-2xl text-black dark:text-black'>
+          Acompañantes Vip
+          </h3>
+          <p className='px-4 sm:px-6 lg:px-8 mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4'>
+          El Acompañante VIP es un entusiasta anfitrión que siempre está dispuesto a brindar hospitalidad a clientes especiales (nacionales y/o internacionales), capaza de hacer disfruta a cualquier cliente/celebritie de su compañía, siempre dispuesto a dar a conocer las diferentes culturas; persona autodidacta y disciplinada que está en constante búsqueda de lugares nuevos, exóticos y fascinantes, que enriquezcan su repertorio y así poder brindar experiencias inolvidables a nuestros clientes.          </p>
+          <p className='px-4 sm:px-6 lg:px-8 mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4'>
+          <h3 className='py-2 text-3xl tracking-tight font-bold sm:text-2xl text-black dark:text-black'>
+          Merchandiser and Stoker
+          </h3>
+          Se ocupan de los inventarios en tiempos breves: disponemos de un personal encargado de la gestión almacenes, reorganización de los displays, preparación y montaje de materiales, control e inventario del almacén.          </p>
         </div>
       </div>
     </div>
