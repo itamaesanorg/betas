bdddbcec26bb2e307a662be5286a35c46497dd43
Miguel Gargallo <mgargallo16@gmail.com>
2022-08-26 17:54:01 +0200

    13.

--- next.config.js
+++ next.config.js
@@ -8,11 +8,6 @@ module.exports = {
   nextConfig,
   images: {
     domains: ['upload.wikimedia.org'],
-    formats: ['image/svg.png+', 'image/png', 'image/jpeg'],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    minimumCacheTTL: 60,
-    dangerouslyAllowSVG: true,
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    formats: ['image/webp', 'image/jpeg', 'image/png'],
   },
 }
