dfe5980153648057716bd536e8e126d68265271d
Miguel Gargallo <mgargallo16@gmail.com>
2022-08-26 19:22:44 +0200

    1.7.5 Update

--- components/uncommon/01 Home/07 Error/02 Body/01 Error/Error.tsx
+++ components/uncommon/01 Home/07 Error/02 Body/01 Error/Error.tsx
@@ -1,7 +1,8 @@
 /* eslint-disable @next/next/no-img-element */
 // Welcome to SushiJS at SushiJS.com by ITAMAESAN at Itamaesan.org
-import Link from 'next/link'
 import React from 'react'
+
+import Logo from '../../../../common/components/01 Head/03 Logo'
 
 export default function Error() {
   return (
@@ -10,17 +11,7 @@ export default function Error() {
           <main className="flex-grow flex flex-col bg-white">
             <div className="flex-grow mx-auto max-w-7xl w-full flex flex-col px-4 sm:px-6 lg:px-8">
               <div className="flex  flex-cflex-shrink-0 pt-10 sm:pt-16">
-              <Link href='/#'>
-            <a className='hover:text-primary transition-all cursor-pointer'>
-              <div className='col-span-1 flex justify-center md:col-span-3 lg:col-span-1'>
-                <img
-                  className='h-24'
-                  src='https://raw.githubusercontent.com/miguelgargallo/SushiJS-Example-001/cf74497949cad84b0544e17b840cd5165c37545a/public/static/favicons/logo.svg'
-                  alt='BCN Hostess'
-                />
-              </div>
-            </a>
-          </Link>
+              <Logo />
               </div>
               <div className="flex-shrink-0 my-auto py-16 sm:py-32">
                 <p className="text-sm font-semibold text-orange-500 uppercase tracking-wide">404 error</p>
@@ -38,11 +29,7 @@ export default function Error() {
           </main>      
         </div>
         <div className="hidden lg:block lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-          <img
-            className="absolute inset-0 h-full w-full object-cover"
-            src="https://images.unsplash.com/photo-1470847355775-e0e3c35a9a2c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1825&q=80"
-            alt=""
-          />
+        <Image />
         </div>
       </div>
   )
