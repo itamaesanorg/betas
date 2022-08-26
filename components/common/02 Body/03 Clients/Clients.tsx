f411253c45cf5999413aca7a7e28d4f0c26b8b31
Miguel Gargallo <mgargallo16@gmail.com>
2022-08-26 10:40:20 +0200

    1.0.0

--- components/common/02 Body/03 Clients/Clients.tsx
+++ components/common/02 Body/03 Clients/Clients.tsx
@@ -126,7 +126,6 @@ export default function Example() {
     <div className="bg-white">
       <div className="mx-auto py-12 px-4 max-w-7xl sm:px-6 lg:px-8 lg:py-24">
         <div className="space-y-12 lg:grid lg:grid-cols-3 lg:gap-8 lg:space-y-0">
-          
           <div className="lg:col-span-3">
             <ul
               role="list"
