f05ad27968c1cc0cf5504c368f525fe0d79c0110
Miguel Gargallo <mgargallo16@gmail.com>
2022-08-29 15:30:01 +0200

    s

--- pages/contacto/inscribete.tsx
+++ pages/contacto/inscribete.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
 
 import Menu from '../../components/common/01 Head/01 Menu'
-import Info from '../../components/uncommon/01 Home/04 Contacto/02 Body/04 info/info'
+import Info from '../../components/uncommon/01 Home/04 Contacto/02 Body/04 info/'
 
 export default function Home() {
   return (
