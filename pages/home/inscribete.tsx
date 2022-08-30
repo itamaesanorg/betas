22cac166e8d66f541190c9ec1cfbc38f9bb41800
Miguel Gargallo <mgargallo16@gmail.com>
2022-08-30 14:18:51 +0200

    a

--- pages/home/inscribete.tsx
+++ pages/home/inscribete.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
 
 import Menu from '../../components/common/01 Head/01 Menu'
-import Info from '../../components/uncommon/01 Home/00 Index/01 Inscribete/02 Body/04 info/info'
+import Info from '../../components/uncommon/01 Home/08 Inscribete/02 Body/04 info/info'
 import Footer from '../../components/common/03 Footer/25 Footer'
 
 export default function Home() {
