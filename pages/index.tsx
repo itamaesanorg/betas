ea5e64379cfdefba55f12746c5aa2c98a6ade308
Miguel Gargallo <mgargallo16@gmail.com>
2022-08-30 14:21:09 +0200

    s

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from 'next'
 import Menu from '../components/common/01 Head/01 Menu'
 import Hero from '../components/common/01 Head/02 Hero'
 import Clients from '../components/common/02 Body/03 Clients'
-import Well from '../components/uncommon/01 Home/00 Index/02 Body/03 Well'
+// import Well from '../components/uncommon/01 Home/00 Index/02 Body/03 Well'
 import Footer from '../components/common/03 Footer/25 Footer'
 
 export default function Home() {
@@ -11,7 +11,6 @@ export default function Home() {
     <>
     <Menu />
     <Hero />
-    <Well />
     <Clients />
     <Footer />
     </>
